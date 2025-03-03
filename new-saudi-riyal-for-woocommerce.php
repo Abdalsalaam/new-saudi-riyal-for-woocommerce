9cce96086dcfc19d76c32067d152162294440211
Abdalsalaam Halawa <abdalsalaamnafez@gmail.com>
2025-03-03 14:35:39 +0400

    Add plugin URL

--- new-saudi-riyal-for-woocommerce.php
+++ new-saudi-riyal-for-woocommerce.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Plugin Name: New Saudi Riyal symbol for WooCommerce
+ * Plugin URI: https://wordpress.org/plugins/saudi-riyal-symbol-for-woocommerce
  * Description: Ensure your store use the new Saudi Riyal symbol.
  * Version: 1.2
  * Author: Abdalsalaam Halawa
